db31ac7f1151ece9d962af6cf6e5f5be5ef9bd00
nathsagar96 <nathsagar96@gmail.com>
2020-09-11 17:42:42 +0530

    project files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.modal').modal();
+  $('.sidenav').sidenav();
+  $('.slider').slider({ full_width: true });
+  $('.parallax').parallax();
+  $('.reviews').carousel({ numVisible: 7, shift: 55, padding: 55 });
+});
+
+function toggleModal() {
+  var instance = M.Modal.getInstance($('#modal3'));
+  instance.open();
+}
